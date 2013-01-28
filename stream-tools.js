e12f934dba0ebca958f037fbb703d39d303435f1
Gabriel Lesperance <gabriel@wavo.me>
2013-01-28 14:27:46 -0500

    FIXED - Missing IdentityBolt from export

--- stream-tools.js
+++ stream-tools.js
@@ -1,9 +1,12 @@
 module.exports = {
     AccumulatorSink : require('./lib/accumulator.sink')
   , AggregateBolt   : require('./lib/aggregate.bolt')
+  , BaseBolt        : require('./lib/base_bolt')
   , CountBolt       : require('./lib/count.bolt')
   , FilterBolt      : require('./lib/filter.bolt')
-  , InOrderMapBolt  : require('./lib/in_order.bolt')
+  , IdentityBolt    : require('./lib/identity.bolt')
+  , InOrderBolt     : require('./lib/in_order.bolt')
+  , InOrderMapBolt  : require('./lib/in_order_map.bolt')
   , StaticSpout     : require('./lib/static.spout')
   , ToJSONBolt      : require('./lib/to_json.bolt')
   , UniqBolt        : require('./lib/uniq.bolt')
